8d6232d21307e918a70fbbd8449cda0ff8800561
leomuko <kezzieleo@gmail.com>
2022-06-24 14:54:39 +0300

    Implement main.js component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,16 @@
 
 function Main(){
     return(
-        <h1>Main Goes Here</h1>
+       <main>
+            <h1 className="main--title">Fun Fact About React</h1>
+            <ul className="main--facts">
+                <li>Was First Released in 2013</li>
+                <li>Was Originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintained by facebook</li>
+                <li>Powers thousands of enterprise apps including mobile apps</li>
+            </ul>
+       </main>
     )
 }
 
